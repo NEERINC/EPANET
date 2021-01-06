7010b7170b04d004ad104c6025677bc86a0f3259
Matthew Downs <matthew@neer.ai>
2021-01-06 09:52:20 -0600

    Update CODEVERSION and build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ cp -R ../src .
 cd ./src
 #sed -i "" 's/#define DLL/\/\/#define DLL/' ./epanet.c
 #sed -i "" 's/\/\/#define CLE/#define CLE/' ./epanet.c
-gcc -Wall main.c epanet.c input1.c input2.c input3.c rules.c output.c report.c inpfile.c hydraul.c smatrix.c quality.c mempool.c hash.c -o epanet2.1.0 -lm
+gcc -Wall main.c epanet.c input1.c input2.c input3.c rules.c output.c report.c inpfile.c hydraul.c smatrix.c quality.c mempool.c hash.c -o epanet2.1.1 -lm
 cd ..
-mv ./src/epanet2.1.0 .
+mv ./src/epanet2.1.1 .
 rm -rf ./src
